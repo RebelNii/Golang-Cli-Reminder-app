33dcab1cf101cffb230741dd980b13dd45ad2f7c
Kwade Jeffrey <kwadejeffrey@gmail.com>
2022-10-29 10:56:57 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+###
+
+THis is a simple Golang CLI reminder app
+
+###
+
+After years of working with Javascript and PHP, I decided to add another programming language to my skills. Since both Javascript and PHP are dynamically typed, I decided to add a static typed language.
+Go won after deliberating between Golang and C++
+
+###
+
+SO here's the first of many go projects
+
+###
+IMPORTED PACKAGES
+
+"github.com/gen2brain/beeep"
+"github.com/olebedev/when"
+"github.com/olebedev/when/rules/common"
+"github.com/olebedev/when/rules/en"
+
+### 
---
+++ main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+	"time"
+
+	"github.com/gen2brain/beeep"
+	"github.com/olebedev/when"
+	"github.com/olebedev/when/rules/common"
+	"github.com/olebedev/when/rules/en"
+)
+
+const (
+	markName  = "Goland CLI reminder"
+	markValue = "1"
+)
+
+func main() {
+	if len(os.Args) < 3 {
+		fmt.Printf("Usage: %v <hh:mm> <text message>\n", os.Args[0])
+		os.Exit(1)
+	}
+
+	now := time.Now()
+
+	w := when.New(nil)
+
+	w.Add(en.All...)
+	w.Add(common.All...)
+
+	t, err := w.Parse(os.Args[1], now)
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(2)
+	}
+
+	if t == nil {
+		fmt.Println("Unable to parse time arg")
+		os.Exit(2)
+	}
+
+	if now.After(t.Time) {
+		fmt.Println("Set a future time args")
+		os.Exit(3)
+	}
+
+	var diff = t.Time.Sub(now)
+
+	if os.Getenv(markName) == markValue {
+		time.Sleep(diff)
+		err = beeep.Alert("Reminder: ", strings.Join(os.Args[2:], " "), "assets/information.png")
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(4)
+		}
+	} else {
+		cmd := exec.Command(os.Args[0], os.Args[1:]...)
+		cmd.Env = append(cmd.Environ(), fmt.Sprintf("%v=%v", markName, markValue))
+		if err := cmd.Start(); err != nil {
+			fmt.Println(err)
+			os.Exit(5)
+		}
+
+		fmt.Printf("Reminder will be display after %v", diff.Round(time.Second))
+		os.Exit(0)
+	}
+
+}
